b293dbd844d726056387f9d4618334d51a4fa3da
Ivan I. Atanasov <i.atanasov77@gmail.com>
2025-01-10 12:49:06 +0000

    Use Tables Bundle Config Option in Backup Command.

--- src/Command/BackupDatabasesCommand.php
+++ src/Command/BackupDatabasesCommand.php
@@ -140,7 +140,7 @@ final class BackupDatabasesCommand extends Command
                 $filePath = "$backupDirectory/$backupName-$database-$date.sql";
 
                 $process = Process::fromShellCommandline(
-                    '"${:MYSQL_DUMP}" -u "${:DB_USER}" -h "${:DB_HOST}" -P "${:DB_PORT}" "${:DB_NAME}" "${:DB_TABLES}" > "${:FILEPATH}"'
+                    '"${:MYSQL_DUMP}" -u "${:DB_USER}" -h "${:DB_HOST}" -P "${:DB_PORT}" "${:DB_NAME}" ${:DB_TABLES} > "${:FILEPATH}"'
                     );
                 
                 $process->setPty(Process::isPtySupported());
